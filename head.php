72f50aabd7097af74fc9d9f694738b8aaf8e9457
Luke Ogburn <21106956+OakBaron@users.noreply.github.com>
2019-05-03 20:21:18 -0400

    Got rid of dependancies on mobileDetect (which was removed a while ago)

--- head.php
+++ head.php
@@ -1,17 +1,7 @@
-<?php
-  require '../res/mobiledetect.php';
-  $detect = new Mobile_Detect;
-  $check = $detect->isMobile(); 
-  if($check){
-    $css = "mobile.css";
-  } else{
-    $css = "index.css";
-  } 
-?>
 <head>
 <meta charset="utf-8">
  <title>Rubiks My Cube | <?php echo $t; ?></title>
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="<?php echo $css; ?>">
+    <link rel="stylesheet" href=index.css>
     <link href="https://fonts.googleapis.com/css?family=Cutive+Mono|PT+Sans|Share+Tech+Mono|Yantramanav:300" rel="stylesheet">
 </head>
